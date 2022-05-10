7dff90e5528315433440cc4abc0624593310aab3
A3zuta <c6101569@planet.kanazawa-it.ac.jp>
2022-05-10 17:10:30 +0900

    ボクセル化のプログラムとサンプル点群データ(.pcd)の追加

--- Voxelization_of_pointCloud.py
+++ Voxelization_of_pointCloud.py
@@ -12,7 +12,3 @@ o3d.visualization.draw_geometries([voxel])
 
 voxel = o3d.geometry.VoxelGrid.create_from_point_cloud(pointCloud, 0.0005)   # ボクセルのサイズ設定
 o3d.visualization.draw_geometries([voxel])
-
-sakatani_PointCloud = o3d.io.read_point_cloud('../open3d_data/sakatani.pcd')
-sakatani_voxel = o3d.geometry.VoxelGrid.create_from_point_cloud(sakatani_PointCloud, 0.005) 
-o3d.visualization.draw_geometries([sakatani_voxel])
